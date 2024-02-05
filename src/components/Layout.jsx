20f8dbe8f3a1f69dd56eb365204ba704a3e14f6a
Junayed Jihad <junayedjihad48@gmail.com>
2024-02-05 22:55:27 +0600

    First Commit

---
+++ src/components/Layout.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+
+function Layout() {
+  return (
+    <div>
+          <div className='banner'>
+               <p className='min-h-[40vh] text-white text-xl sm:text-2xl md:text-3xl flex justify-center items-center'>Nanomaterials and Ceramic Engineering</p>
+          </div>
+          <div className='min-h-[75vh] py-6'>
+               <Outlet/>
+          </div>
+          <div className='h-36 sm:h-40 md:h-52 flex justify-center items-center bg-slate-100'>
+               <p className='font-medium text-2xl'>Best wishes from NCE'22</p>
+          </div>
+
+
+    </div>
+  )
+}
+
+export default Layout
